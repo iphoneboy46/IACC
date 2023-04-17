a5b647c1fd0a884432c17d2eab6fa8379f8d5def
chotr <minhphungbmt141999@gmail.com>
2023-04-17 17:46:34 +0700

    js form

--- template/js/module/input.js
+++ template/js/module/input.js
@@ -22,7 +22,6 @@ export default function inputForm() {
       });
     }
 
-
     const showRe = document.querySelector(".re-hide-pass");
     const hiddenRe = document.querySelector(".re-show-pass");
     const inputPassRe = document.querySelector(".re-pass");
@@ -45,6 +44,42 @@ export default function inputForm() {
       });
     }
 
+    //////////////////////////////////////////////////////////////
+    ////form edit
+
+    const form = document.querySelectorAll(".form-edit");
+
+    if (form) {
+      form.forEach((ele, i) => {
+        const btn = ele.querySelector(".form-edit-btn");
+        const inputList = ele.querySelectorAll("input");
+        const recheck = ele.querySelectorAll(".recheck-item")
+
+        recheck.forEach((item) => {
+          item.preventDefault();
+        })
+
+        inputList.forEach((input, i) => {
+          input.disabled = true;
+        })
+
+        btn.addEventListener("click", () => {
+          ele.classList.add("active");
+          if (ele.classList.contains("active")) {
+            const control = ele.querySelectorAll(".form-edit-control");
+
+            control.forEach((ctEl, j) => {
+              const edit = ctEl.querySelector(".form-edit-icon");
+              const input = ctEl.querySelector(".form-edit-input");
+              input.removeAttribute("disabled");
+              edit.addEventListener("click", () => {
+                input.focus();
+              });
+            });
+          }
+        });
+      });
+    }
   } catch (error) {
     console.log(error);
   }
